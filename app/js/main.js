1e11c1e1b30ced69a8b50207321942398d11a39c
tawshif <anikphp@gmail.com>
2016-12-16 23:17:35 +0600

    some custom forms element added

--- app/js/main.js
+++ app/js/main.js
@@ -21,7 +21,6 @@ var pageCounter = 1;
 var btn = document.getElementById('btn');
 var animalContainer = document.getElementById('animal');
 
-
 btn.addEventListener("click", function(){
 	var ourRequest = new XMLHttpRequest();
 	ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-'+ pageCounter +'.json');
@@ -29,7 +28,6 @@ btn.addEventListener("click", function(){
 		var ourData = JSON.parse(ourRequest.responseText);
 		renderHtml(ourData);
 	};
-
 	ourRequest.send();
 	pageCounter ++;
 
@@ -39,22 +37,17 @@ btn.addEventListener("click", function(){
 });
 
 function renderHtml(data){
-	
 	var htmlString = "";
 	for (var i = 0; i < data.length; i++) {
 		htmlString += "<li class='mdl-list__item'>"+ data[i].name+ " is a " + data[i].species +" that likes the ";
-
 		for (var ii = 0; ii < data[i].foods.likes.length; ii++) {
-
 			if (ii === 0) {
 				htmlString += data[i].foods.likes[ii];
 			} else {
 				htmlString += " and "+ data[i].foods.likes[ii];
 			}
 		}
-
 		htmlString += " and dislikes ";
-
 		for (var iii = 0; ii < data[i].foods.dislikes.length; iii++) {
 
 			if (iii === 0) {
@@ -65,8 +58,6 @@ function renderHtml(data){
 		}
 		htmlString += " .</li>";
 	}
-
-	
 	animalContainer.insertAdjacentHTML("afterbegin", htmlString);
 
 }
